f840b8aefdf5d5ea9b8c980300810c06021af78d
denisunderonov <denisunderonov2@gmail.com>
2025-12-03 12:57:16 +0300

    Андронов

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,1 +1,34 @@
 export type sum = (a: number, b: number) => number
+
+export interface StringValidator {
+  min(minLength: number, message?: string): StringValidator;
+  max(maxLength: number, message?: string): StringValidator;
+  required(message?: string): StringValidator;
+  email(message?: string): StringValidator;
+  pattern(regex: RegExp, message?: string): StringValidator;
+}
+
+export interface NumberValidator {
+  min(minValue: number, message?: string): NumberValidator;
+  max(maxValue: number, message?: string): NumberValidator;
+  required(message?: string): NumberValidator;
+}
+
+export interface ArrayValidator {
+  min(minItems: number, message?: string): ArrayValidator;
+  max(maxItems: number, message?: string): ArrayValidator;
+  required(message?: string): ArrayValidator;
+}
+
+export interface FieldValidator {
+  string(): StringValidator;
+  number(): NumberValidator;
+  array(): ArrayValidator;
+}
+
+export interface FormValidator {
+  field(fieldName: string): FieldValidator;
+  validate(): boolean;
+}
+
+export type FormFactoryFunction = (formElement: HTMLFormElement) => FormValidator;
